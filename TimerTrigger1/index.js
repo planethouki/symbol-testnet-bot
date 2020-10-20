6f03d1ec4ec240f99ca8c60290251e715c1c6ee8
planethouki <26595148+planethouki@users.noreply.github.com>
2020-10-21 00:21:09 +0900

    height

--- TimerTrigger1/index.js
+++ TimerTrigger1/index.js
@@ -1,4 +1,11 @@
 const Twitter = require('twitter');
+const axios = require('axios');
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
 
 module.exports = function (context, myTimer) {
     const timeStamp = new Date().toISOString();
@@ -7,27 +14,36 @@ module.exports = function (context, myTimer) {
     {
         context.log('JavaScript is running late!');
     }
-    context.log('JavaScript timer trigger function ran!', timeStamp); 
+    context.log('JavaScript timer trigger function ran!', timeStamp);
 
-    const client = new Twitter({
+    const nodeList = process.env.NODES_CSV.split(",");
+    const node = nodeList[getRandomInt(0, nodeList.length)]
+    axios.default.baseURL = node;
+
+    const twitter = new Twitter({
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
         consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
         access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
         access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
     });
-
-    client.post(
-        'statuses/update',
-        {status: 'Now: ' + new Date()},
-        function(error, tweet, response) {
-            if (error) {
-                context.log(error);
-            };
-            if (process.env.NODE_ENV === 'development') {
-                context.log(tweet);
-                context.log(response);
+    
+    axios.get("/chain/info").then((res) => {
+        return res.data.height
+    }).then((height) => {
+        twitter.post(
+            'statuses/update',
+            {status: 'Current Height: ' + height},
+            function(error, tweet, response) {
+                if (error) {
+                    context.log(error);
+                };
+                if (process.env.NODE_ENV === 'development') {
+                    context.log(tweet);
+                    context.log(response);
+                }
+                context.done();
             }
-            context.done();
-        }
-    );  
+        );
+    })
+
 };
