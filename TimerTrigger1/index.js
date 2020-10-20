8d815a6172e76439397f0c4f13d73825bd3914a1
planethouki <26595148+planethouki@users.noreply.github.com>
2020-10-20 23:17:46 +0900

    commit

--- TimerTrigger1/index.js
+++ TimerTrigger1/index.js
@@ -1,5 +1,24 @@
+const Twitter = require('twitter');
+
 module.exports = async function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
+    const timeStamp = new Date().toISOString();
+
+    const client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    });
+
+    client.post(
+        'statuses/update',
+        {status: 'Now: ' + new Date()},
+        function(error, tweet, response) {
+            if(error) throw error;
+            console.log(tweet);
+            console.log(response);
+        }
+    );
     
     if (myTimer.isPastDue)
     {
