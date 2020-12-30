2633b00cfd8c0ee3b9a575bae1be669190bb2a38
planethouki <26595148+planethouki@users.noreply.github.com>
2020-12-30 14:17:01 +0900

    retry

--- SingleNodeStorage/index.js
+++ SingleNodeStorage/index.js
@@ -24,9 +24,6 @@ module.exports = function (context, myTimer) {
         { url: "http://api-01.us-east-1.0.10.0.x.symboldev.network:3000", name: "us-east-1" },
         { url: "http://api-01.us-west-1.0.10.0.x.symboldev.network:3000", name: "us-west-1" }
     ];
-    const node = nodeList[getRandomInt(0, nodeList.length)].url;
-    context.log(`node: ${node}`);
-    axios.defaults.baseURL = node;
 
     const twitter = new Twitter({
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -34,32 +31,49 @@ module.exports = function (context, myTimer) {
         access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
         access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
     });
+
+    for (let i = 0; i < nodeList.length; i++) {
+        const node = nodeList[getRandomInt(0, nodeList.length)].url;
+        context.log(`node: ${node}`);
     
-    axios
-        .get("/node/storage", { timeout: 5000 })
-        .then((res) => {
-            context.log(res.data);
-            return res.data
-        })
-        .then(({numBlocks, numTransactions, numAccounts}) => {
-            twitter.post(
-                'statuses/update',
-                {status: `Symbol Testnet\nNum Blocks: ${numBlocks}\nNum Transactions: ${numTransactions}\nNum Accounts: ${numAccounts}`},
-                function(error, tweet, response) {
-                    if (error) {
-                        context.log(error);
-                    };
-                    if (process.env.NODE_ENV === 'development') {
-                        context.log(tweet);
-                        context.log(response);
-                    }
-                    context.done();
-                }
-            );
-        })
-        .catch((e) => {
-            context.log(e);
-            context.done();
-        })
+        const isSuccess = await axios
+            .get(`${node}/node/storage`, { timeout: 5000 })
+            .then((res) => {
+                context.log(res.data);
+                return res.data
+            })
+            .then(({numBlocks, numTransactions, numAccounts}) => {
+                return new Promise((resolve, reject) => {
+                    twitter.post(
+                        'statuses/update',
+                        {status: `Symbol Testnet\nNum Blocks: ${numBlocks}\nNum Transactions: ${numTransactions}\nNum Accounts: ${numAccounts}`},
+                        function(error, tweet, response) {
+                            if (error) {
+                                context.log.error(error);
+                                return reject();
+                            };
+                            if (process.env.NODE_ENV === 'development') {
+                                context.log(tweet);
+                                context.log(response);
+                            }
+                            resolve();
+                        }
+                    );
+                })
+            })
+            .then(() => {
+                return true;
+            })
+            .catch((e) => {
+                context.log.error(e);
+                return false;
+            });
+
+        if (isSuccess) {
+            context.log("success")
+            break;
+        }
+    }
+    
 
 };
